08adaf14574a88c0f4c4f13423edb25d6a98287d
Jakub <122922171+K4NDR0S@users.noreply.github.com>
2024-06-06 12:22:44 +0200

    updated password for database

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ const connection = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: "",//add here password from .env by adding process.env.DB_PASSWORD
   database: process.env.DB_NAME,
 });
 

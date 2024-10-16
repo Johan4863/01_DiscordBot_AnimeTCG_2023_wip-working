ddf817dc25062f321f196eae0f8fff82d53726fa
Johan <122922171+Johan4863@users.noreply.github.com>
2024-10-16 13:45:11 +0200

    modules update

---
+++ moderators.mjs
@@ -0,0 +1,16 @@
+export const ModeratorUsers = [
+  {
+    "id": "881351129995284530",
+    "usertag": "gama"
+  },
+  {
+    "id": "556469452003344384",
+    "usertag": "kandros"
+  },
+  {
+    "id": "1230728168458162216",
+    "usertag": "johan095137"
+  }
+]
+
+export default ModeratorUsers;
